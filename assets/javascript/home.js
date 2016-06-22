8682ef6a1f569e0ee93d191f11776376062bca55
Laura <lauralizgarciag@gmail.com>
2016-06-21 23:37:48 -0500

    Se agrego una validacion para el correo en home.js

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -3,11 +3,19 @@ function funcion()
 	var formulario = document.getElementsByName('forRegistro'),
 		boton = document.getElementsByName('Ingresar'),
 		carContrasena = /^([a-zA-Z0-9]{6,20})$/;
+	   	carCorreo = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
 	
 	var validarCorreo = function(e){
 		if(formulario[0].logCorreo.value == ""){
 			alert("Completa el campo correo");
 			e.preventDefault();
+		}
+		else{
+			if(!carCorreo.test(formulario[0].logCorreo.value))
+			{
+				alert("Correo no valido");
+				e.preventDefault();
+			}
 		}
 	};
 	
