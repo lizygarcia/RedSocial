6a24db86a24f21e30795522d2624714d198ca1ad
Laura <lauralizgarciag@gmail.com>
2016-06-22 19:45:25 -0500

    Se añadio la funcion validarRegistro en home.js

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,9 +1,9 @@
 function validarEntrada()
 {
-	var formulario = document.getElementsByName('forRegistro'),
+	var formulario = document.getElementsByName('forEntrada'),
 		boton = document.getElementsByName('Ingresar'),
-		carContrasena = /^([a-zA-Z0-9]{6,20})$/;
-	   	carCorreo = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
+		carContrasena = /^([a-zA-Z0-9]{6,20})$/,
+	   	carCorreo = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
 	
 	var validarCorreo = function(e){
 		if(formulario[0].logCorreo.value == ""){
@@ -53,3 +53,116 @@ function validarEntrada()
 	formulario[0].addEventListener("submit",validar);
 	
 }
+
+function validarRegistro()
+{
+	var formulario = document.getElementsByName('forRegistro'),
+		boton = document.getElementsByName('Registrarse'),
+		carContrasena = /^([a-zA-Z0-9]{6,20})$/,
+	   	carCorreo = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/,
+		carAlfabetico = /^([a-zA-Z]+[\s]*)+$/;
+	
+	var validarNombre = function(e){
+		if(formulario[0].regNombre.value == "")
+		{
+			alert("Completa el campo Nombre");
+			e.preventDefault();
+		}
+		else{
+			if(!carAlfabetico.test(formulario[0].regNombre.value))
+			{
+				alert("Nombre no valido");
+				e.preventDefault();
+			}
+		}
+		
+	};
+	
+	var validarApellido = function(e){
+		if(formulario[0].regApellido.value == "")
+		{
+			alert("Completa el campo Apellido");
+			e.preventDefault();
+		}
+		else{
+			if(!carAlfabetico.test(formulario[0].regApellido.value))
+			{
+				alert("Apellido no valido");
+				e.preventDefault();
+			}
+		}
+	};
+	
+	var validarCorreo = function(e){
+		if(formulario[0].regCorreo.value == ""){
+			alert("Completa el campo correo");
+			e.preventDefault();
+		}
+		else{
+			if(!carCorreo.test(formulario[0].regCorreo.value))
+			{
+				alert("Correo no valido");
+				e.preventDefault();
+			}
+		}
+	};
+	
+	var validarContrasena = function(e){
+		if(formulario[0].regContrasena.value == ""){
+			alert("Completa el campo contraseña");
+			e.preventDefault();
+		}
+		else{
+			if(!carContrasena.test(formulario[0].regContrasena.value)){
+				alert("Contraseña no valida");
+				e.preventDefault();
+			}
+		}
+	};
+
+	var validarRptContrasena = function(e){
+		if(formulario[0].regConfirmaContrasena.value == ""){
+			alert("Completa el campo contraseña");
+			e.preventDefault();
+		}
+		else{
+			if(!carContrasena.test(formulario[0].regConfirmaContrasena.value)){
+				alert("Contraseña no valida");
+				e.preventDefault();
+			}
+		}
+		
+	};
+	
+	var validarLasContrasenas = function(e){
+		if(formulario[0].regContrasena.value != formulario[0].regConfirmaContrasena.value){
+			alert("Las contraseñas no coinciden");
+			e.preventDefault();
+		}
+
+		
+	};
+	
+	
+	var validadTerCond = function(e)
+	{
+		if(formulario.TerCond.checked == false{
+			alert("Debe aceptar los terminos y condiciones");
+			e.preventDefault();
+		}
+
+	};
+	
+	var validar = function(e){
+		validarNombre(e);
+		validarApellido(e);
+		validarCorreo(e);
+		validarContrasena(e);	
+		validarRptContrasena(e);
+		validarTerCond(e);
+	};
+	
+	formulario[0].addEventListener("submit",validar);
+	
+}
+
